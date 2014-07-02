b13cb9acfb87f62e5e188ff146a8aa0f63595b34
Steven Crawford <crawfordsm@gmail.com>
2014-07-02 18:24:02 -0400

    add script to get sloan templates

---
+++ zsalt/gettemplates.sh
@@ -0,0 +1,6 @@
+
+#Shell script for getting the SDSS templates
+mkdir template
+cd template
+wget http://classic.sdss.org/dr7/algorithms/spectemplates/spectemplatesDR2.tar.gz
+tar zxvf spectemplatesDR2.tar.gz
