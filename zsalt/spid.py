ad7ef09d48ab4e051d62c07a45b9a763ce899f45
Steven Crawford <crawfordsm@gmail.com>
2015-11-20 14:24:28 +0200

    updated version of spid.py

--- zsalt/spid.py
+++ zsalt/spid.py
@@ -4,5 +4,10 @@ from pyraf import iraf
 from iraf import pysalt
 from specidentify import specidentify
 
-specidentify(images=sys.argv[1], linelist=sys.argv[2], outfile='wav.db', guesstype='rss', guessfile='', automethod='Matchlines', function='poly', order=3, rstep=100, rstart='middlerow', mdiff=20, thresh=3, niter=5, smooth=3, inter=True, startext=0 ,clobber=True, textcolor='black', logfile='salt.log',
+from astropy.io import fits
+
+header = fits.open(sys.argv[1])[0].header
+lampid = header['LAMPID'].strip().replace(' ', '')
+print lampid
+specidentify(images=sys.argv[1], linelist='/Users/crawford/research/kepler/%s.salt' % lampid, outfile='wav.db', guesstype='rss', guessfile='', automethod='Matchlines', function='poly', order=3, rstep=100, rstart='middlerow', mdiff=20, thresh=3, niter=5, smooth=3, inter=True, startext=0 ,clobber=True, textcolor='black', logfile='salt.log',
 verbose=True)
