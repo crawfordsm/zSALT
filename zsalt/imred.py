0b9cc7afe4d57c27789bab6f4faf95e3ea429af4
Steven Crawford <crawfordsm@gmail.com>
2015-12-23 13:34:58 -0600

    added updates from khn

--- zsalt/imred.py
+++ zsalt/imred.py
@@ -34,7 +34,7 @@ from saltflat import saltflat
 from saltmosaic import saltmosaic
 from saltillum import saltillum
 
-def imred(infile_list, prodir, bpmfile=None, gaindb = None, cleanup=True):
+def imred(infile_list, prodir, bpmfile=None, gaindb = None, geomfile = None, cleanup=True):
 
     #get the name of the files
     infiles=','.join(['%s' % x for x in infile_list])
@@ -98,16 +98,32 @@ def imred(infile_list, prodir, bpmfile=None, gaindb = None, cleanup=True):
     for i in range(len(infile_list)):
         if obs_dict['CCDTYPE'][i].count('OBJECT') and obs_dict['INSTRUME'][i].count('RSS'):
           img='fxgbp'+os.path.basename(infile_list[i])
-          saltcrclean(img, img, '', crtype='edge', thresh=5, mbox=11, bthresh=5.0,
-                flux_ratio=0.2, bbox=25, gain=1.0, rdnoise=5.0, fthresh=5.0, bfactor=2,
-                gbox=3, maxiter=5, multithread=True,  clobber=True, logfile=logfile, verbose=True)
+          #saltcrclean(img, img, '', crtype='edge', thresh=5, mbox=11, bthresh=5.0,
+          #      flux_ratio=0.2, bbox=25, gain=1.0, rdnoise=5.0, fthresh=5.0, bfactor=2,
+          #      gbox=3, maxiter=5, multithread=True,  clobber=True, logfile=logfile, verbose=True)
 
     #mosaic the data
-    geomfile=iraf.osfn("pysalt$data/rss/RSSgeom.dat")
+    if geomfile is None: geomfile=iraf.osfn("pysalt$data/rss/RSSgeom.dat")
     try:
        saltmosaic('fxgbpP*fits', '', 'm', geomfile, interp='linear', cleanup=True, geotran=True, clobber=True, logfile=logfile, verbose=True)
     except:
        saltmosaic('fxgbpP*fits', '', 'm', geomfile, interp='linear', cleanup=True, geotran=True, clobber=True, logfile=logfile, verbose=True)
+
+    #add needed header columns and make a better mask
+    for img in infile_list:
+        filename = 'mxgbp'+os.path.basename(img)
+        hdu = pyfits.open(filename, 'update')
+        hdu[2].header.update('EXTNAME','VAR')
+        hdu[3].header.update('EXTNAME','BPM')
+        bpm_rc = (hdu[3].data>0).astype('uint8')
+        zeroscicol = hdu['SCI'].data.sum(axis=0) == 0
+        bpmgapcol = bpm_rc.mean(axis=0) == 1
+        addbpmcol = zeroscicol & ~bpmgapcol
+        addbpmcol[np.argmax(addbpmcol)-4:np.argmax(addbpmcol)] = True    # allow for chip tilt
+        bpm_rc[:,addbpmcol] = 1
+        hdu[3].data = bpm_rc
+        hdu.writeto(filename,clobber=True)
+
 
     #clean up the images
     if cleanup:
@@ -120,24 +136,65 @@ def imred(infile_list, prodir, bpmfile=None, gaindb = None, cleanup=True):
 
 def add_variance(filenames, bpmfile):
     file_list=glob.glob(filenames)
-    badpixelstruct = saltio.openfits(bpmfile)
+    badpixelstruct = fits.open(bpmfile)
     for f in file_list:
         struct = fits.open(f)
         nsciext=len(struct)-1
         nextend=nsciext
         for i in range(1, nsciext+1):
             hdu=CreateVariance(struct[i], i, nextend+i)
-            struct[i].header.set('VAREXT',nextend+i, comment='Extension for Variance Frame')
+            hdu.header.update('EXTNAME','VAR')
+            struct[i].header['VAREXT'] = (nextend+i, 'Extension for Variance Frame')
             struct.append(hdu)
         nextend+=nsciext
         for i in range(1, nsciext+1):
-            hdu=createbadpixel(struct, badpixelstruct, i, nextend+i)
-            struct[i].header.set('BPMEXT',nextend+i, comment='Extension for Bad Pixel Mask')
+            if os.path.basename(bpmfile)=='bpm_rss_11.fits':
+                hdu=masterbadpixel(struct, badpixelstruct, i, nextend+i)
+            else:
+                hdu=createbadpixel(struct, badpixelstruct, i, nextend+i)
+            struct[i].header['BPMEXT']=(nextend+i, 'Extension for Bad Pixel Mask')
+            hdu.header.update('EXTNAME','BPM')
             struct.append(hdu)
         nextend+=nsciext
-        struct[0].header.set('NEXTEND', nextend)
+        struct[0].header['NEXTEND'] = nextend
         if os.path.isfile(f): os.remove(f)
         struct.writeto(f)
+
+def masterbadpixel(inhdu, bphdu, sci_ext, bp_ext):
+    """Create the bad pixel hdu bp_ext for inhdu[sci_ext] from a master, bphdu
+    """
+
+    if bphdu is None:
+        data=np.zeros_like(inhdu[sci_ext].data).astype("uint8")
+    else:
+        infile=inhdu.fileinfo(0)['filename']
+        bpfile=bphdu.fileinfo(0)['filename']
+        masternext = len(bphdu)-1
+        masterext = (sci_ext-1) % masternext + 1        # allow for windows
+
+        if not saltkey.compare(inhdu[0], bphdu[0], 'INSTRUME', infile, bpfile):
+            message = '%s and %s are not the same %s' % (infile,bpfile, 'INSTRUME')
+            raise SaltError(message)
+        else:
+            rows,cols = inhdu[sci_ext].data.shape
+            cbin,rbin = np.array(inhdu[sci_ext].header["CCDSUM"].split(" ")).astype(int)
+            masterrows,mastercols = bphdu[masterext].data.shape
+            master_rc = np.ones((masterrows+(masterrows % rbin),mastercols+(mastercols % cbin)))
+            master_rc[:masterrows,:mastercols] = bphdu[masterext].data
+            masterrows,mastercols=(masterrows+(masterrows % rbin),mastercols+(mastercols % cbin))
+            ampsec = inhdu[sci_ext].header["AMPSEC"].strip("[]").split(",")
+            r1,r2 = (np.array(ampsec[1].split(":")).astype(int) / rbin) * rbin
+            c1,c2 = (np.array(ampsec[0].split(":")).astype(int) / cbin) * cbin
+            if c1 > c2: c1,c2 = c2,c1
+            bin_rc = (master_rc.reshape(masterrows/rbin,rbin,mastercols/cbin,cbin).sum(axis=3).sum(axis=1) > 0)
+            data = bin_rc[ r1:r2, c1:c2 ].astype('uint8')
+
+    header=inhdu[sci_ext].header.copy()
+    header.update('EXTVER',bp_ext)
+    header.update('SCIEXT',sci_ext,comment='Extension of science frame')
+
+    return pyfits.ImageHDU(data=data, header=header, name='BPM')
+
 
 if __name__=='__main__':
    raw_files=glob.glob(sys.argv[1]+'P*fits')
