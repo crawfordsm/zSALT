1c3388b0dc444d77a19c4b0c18b8b27ae0dc5937
Steve Crawford <crawfordsm@gmail.com>
2017-08-18 12:23:40 +0200

    updates to spid.py

--- zsalt/spid.py
+++ zsalt/spid.py
@@ -9,5 +9,5 @@ from astropy.io import fits
 header = fits.open(sys.argv[1])[0].header
 lampid = header['LAMPID'].strip().replace(' ', '')
 print lampid
-specidentify(images=sys.argv[1], linelist='/Users/crawford/research/kepler/%s.salt' % lampid, outfile='wav.db', guesstype='rss', guessfile='', automethod='Matchlines', function='poly', order=3, rstep=100, rstart='middlerow', mdiff=20, thresh=3, niter=5, smooth=3, inter=True, startext=0 ,clobber=True, textcolor='black', logfile='salt.log',
+specidentify(images=sys.argv[1], linelist='/Users/crawford/programs/pysalt/data/linelists/%s.salt' % lampid, outfile='wav.db', guesstype='rss', guessfile='', automethod='Matchlines', function='poly', order=3, rstep=100, rstart='middlerow', mdiff=20, thresh=3, niter=5, smooth=5, inter=True, startext=0 ,clobber=True, textcolor='black', logfile='salt.log',
 verbose=True)
