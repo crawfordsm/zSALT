8963648898b7887da3ec1973c5808ff264f61045
Steve Crawford <crawfordsm@gmail.com>
2017-09-13 14:28:25 -0700

    add guessfile option

--- zsalt/reducedata.py
+++ zsalt/reducedata.py
@@ -10,8 +10,14 @@ bpmfile = os.path.dirname(__file__)+'/data/bpm_rss_11.fits'
 
 parser = argparse.ArgumentParser(description='Reduce SALT Lens Data')
 parser.add_argument('ddir', help='Top level directory with SALT data')
+parser.add_argument('-p', dest='preprocess', default=False, action='store_true',
+                    help='prepocess the line identification')
+parser.add_argument('-a', dest='auto', default=False, action='store_true',
+                    help='Autoidentify the lines')
 parser.add_argument('-s', dest='basic_red', default=True, action='store_false',
                     help='Skip basic reduction')
+parser.add_argument('--g', dest='guess_file', default=None, type=str, 
+                    help='Initial guess file for reductions')
 
 args = parser.parse_args()
 ddir = args.ddir
@@ -28,9 +34,8 @@ if args.basic_red: imred(infile_list, './', bpmfile, cleanup=True)
 #spectroscopic reductions
 propid=None
 infile_list = glob.glob('m*fits')
-specred(infile_list, propid, inter=True, guessfile=None)
+specred(infile_list, propid, inter=True, guessfile=args.guess_file, preprocess=args.preprocess)
 
 #extract the data
-
 
 
